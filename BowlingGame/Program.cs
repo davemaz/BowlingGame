f289c119f79aec189728c33a250dac00dc262c7b
David Mazmanian <DavidMazmanian@hotmail.com>
2016-10-24 19:05:24 -0400

    Refactor

--- BowlingGame/Program.cs
+++ BowlingGame/Program.cs
@@ -33,34 +33,27 @@ namespace BowlingGame
     [TestClass]
     public class BowlingTest
     {
-        private Game g;
+        private Game g = new Game();
 
-        [TestMethod]
-        public void canRoll()
+        private void RollMany(int n, int pins)
         {
-            g = new Game();
-            g.Roll(0);
+            for (int i = 0; i < n; i++)
+            {
+                g.Roll(pins);
+            }
         }
 
         [TestMethod]
         public void gutterGame()
         {
-            g = new Game();
-            for (int i = 0; i < 20; i++)
-            {
-                g.Roll(0);
-            }
+            RollMany(20, 0);
             Assert.AreEqual(0, g.Score());
         }
 
         [TestMethod]
         public void allOnes()
         {
-            g = new Game();
-            for (int i = 0; i < 20; i++)
-            {
-                g.Roll(1);
-            }
+            RollMany(20, 1);
             Assert.AreEqual(20, g.Score());
         }
     }
