766d558dcdbc5dd7b231d961f81a219bc68e83a4
David Mazmanian <DavidMazmanian@hotmail.com>
2016-10-24 19:27:56 -0400

    Calculate spare

--- BowlingGame/Program.cs
+++ BowlingGame/Program.cs
@@ -29,9 +29,19 @@ namespace BowlingGame
         public int Score()
         {
             int score = 0;
-            for (int i = 0; i < rolls.Length; i++)
+            int i = 0;
+            for (int frame = 0; frame < 10; frame++)
             {
-                score += rolls[i];
+                if (rolls[i] + rolls[i + 1] == 10) // spare
+                {
+                    score += 10 + rolls[i + 2];
+                    i += 2;
+                }
+                else
+                {
+                    score += rolls[i] + rolls[i + 1];
+                    i += 2;
+                }
             }
             return score;
         }
@@ -64,7 +74,6 @@ namespace BowlingGame
             Assert.AreEqual(20, g.Score());
         }
 
-        [Ignore]
         [TestMethod]
         public void oneSpare()
         {
